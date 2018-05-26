c6be47e0057e1c5c07d8239c1e480131fc4a98cf
Tristan Villamil <villamil101@gmail.com>
2018-05-26 13:45:35 -0400

    try php

---
+++ included/menu.php
@@ -0,0 +1,5 @@
+<ul id="links">
+	<a href="index.html"><li class="menu">Home</li></a>
+	<a href="resume.pdf" target="_blank"><li class="menu">Resume</li></a>
+	<a href="wiki.html"><li class="menu">Knowledge</li></a>
+</ul>
